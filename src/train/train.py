8a0450307d2d88eacc4b59eb27b8458fddece78e
bk073 <karkeebishwa1@gmail.com>
2020-07-09 08:14:30 +0545

    train model
    Script to train the model with small batch size
    
    All the necessary parameters needed to train the model are initialize in this module

---
+++ src/train/train.py
@@ -0,0 +1,26 @@
+from models import base_model
+from data.data_generator import train_data_generator, valid_data_generator, create_dataframe
+import pandas as pd
+import glob as glob
+import  os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+def train():
+    batch_size = 128
+    epochs = 5
+    IMG_HEIGHT = 150
+    IMG_WIDTH = 150
+    img_dir = '/home/atlas/Atlas/Bishwa/data/image'
+    input_shape = (150, 150, 3)
+ 
+    df = create_dataframe(img_dir)
+    print(df.shape)
+    train_data, valid_data = train_test_split(df, test_size=0.2,stratify=df['label'])
+    train_data_gen = train_data_generator(train_data, batch_size = 128, IMG_HEIGHT = 150, IMG_WIDTH = 150, img_dir=img_dir)
+    model = base_model.BaseModel(input_shape, train_data_gen)
+    model.model()
+    model.compile_model()
+    model.fit()
+#  make changes
+
