0ae623a3ce5c08a94f5e8fb8a176270fc49f98fc
bk073 <karkeebishwa1@gmail.com>
2020-07-09 16:50:05 +0545

    Called fit,save and training_log methods
    Called those methods after the model is trained

--- src/train/train.py
+++ src/train/train.py
@@ -15,12 +15,14 @@ def train():
     input_shape = (150, 150, 3)
  
     df = create_dataframe(img_dir)
-    print(df.shape)
     train_data, valid_data = train_test_split(df, test_size=0.2,stratify=df['label'])
     train_data_gen = train_data_generator(train_data, batch_size = 128, IMG_HEIGHT = 150, IMG_WIDTH = 150, img_dir=img_dir)
     model = base_model.BaseModel(input_shape, train_data_gen)
     model.model()
     model.compile_model()
-    model.fit()
-#  make changes
+    model.fit(epochs)
+    model.save()
+    model.training_log()
 
+#  '/home/atlas/Atlas/Bishwa/data/image'
+# /home/bishwa/G/shell-identification/data/new_shell_images_2nd
