b4663af0ac528ff29268ff0e3e343a9dd9d3b780
bishwa <karkeebishwa1@gmail.com>
2020-07-14 20:16:11 +0545

    Added different seperate function for evaluation

--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ from train import train, train_mobile_net
 import evaluate
 
 def main(train_model, model_type): 
+    print(train_model)
     if train_model:
         if model_type == 'base_model':
             train.train()
         if model_type == 'mobile_net':
             train_mobile_net.train()
     else:
-        evaluate.eval()
+        evaluate.eval(model=model_type)
+
+def eval_(model_type):
+    evaluate.eval(model_type=model_type)
 
 
 if __name__ == '__main__':
-    main(train_model=True, model_type='mobile_net')
+#     main(train_model=True, model_type='mobile_net')
+    eval_(model_type='mobile_net')
