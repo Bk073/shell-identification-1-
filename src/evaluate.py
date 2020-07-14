3512a2e61d2d4da6e69afa0728a4d855d642894a
bishwa <karkeebishwa1@gmail.com>
2020-07-14 17:08:44 +0545

    Added evaluation for test for mobile net

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,10 @@
 from models import base_model
 from data.data_generator import test_data_generator, create_dataframe
 from matplotlib import pyplot as plt
-
+import pandas as pd
+from models.mobile_net import Mobile_net
+import tensorflow as tf
+from predict import load_level
 
 def eval_plot(loss, accuracy):
     plt.figure(figsize=(8, 8))
@@ -13,9 +16,30 @@ def eval_plot(loss, accuracy):
     plt.title('Accuracy and loss')
 
 
-def eval():
-    test_data_gen = test_data_generator()
-    base_model = base_model.BaseModel(test_data_gen=test_data_gen)
-    base_model.load_model()
-    loss, accuracy = base_model.eval()
-    eval_plot(loss, accuracy)
+def eval(model):
+    IMG_HEIGHT = 224
+    IMG_WIDTH = 224
+    img_dir = '/home/atlas/Atlas/Bishwa/data/image'
+    test_df = pd.read_csv('/home/atlas/Atlas/Bishwa/shell-identification-1-/src/data/test_data.csv')
+    test_data_gen = test_data_generator(test_df, IMG_HEIGHT = IMG_HEIGHT, IMG_WIDTH = IMG_WIDTH, img_dir=img_dir)
+    if model_type == 'base_model':
+        base_model = base_model.BaseModel(test_data_gen=test_data_gen)
+        base_model.load_model()
+        loss, accuracy = base_model.eval()
+        eval_plot(loss, accuracy)
+        
+    if model_type == 'mobile_net':
+        model = Mobile_net()
+        model.load_weights('/home/atlas/Atlas/Bishwa/shell-identification-1-/models/v1.01.mobile-net/')
+        predict = model.predict(test_data_gen)
+        predict_indices = tf.math.argmax(predict, 1)
+        
+        label_dict_inv = load_level()
+
+        prediction = [] 
+
+        for index in predict_indices:
+            prediction.append(label_dict_inv[index.numpy()])
+        
+        print(accuracy_score(prediction, test_df['label']))
+        print(classification_report(test_df['label'], prediction))
